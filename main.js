a33cf9fe4d6b1fbf48bf339e6e1749528b73f500
bhandariarnav2020 <76773711+bhandariarnav2020@users.noreply.github.com>
2021-07-17 17:06:46 +0530

    Create main.js

---
+++ main.js
@@ -0,0 +1,8 @@
+password=""
+function onclick() {
+  password=document.getElementById("password").value;
+  if(password = "Space213") {
+    window.location='https://bhandariarnav2020.github.io/SRA/';
+  }
+    
+    
