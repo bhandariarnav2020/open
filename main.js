a82f953ecad72c869ffba9032eefb461582b2891
bhandariarnav2020 <76773711+bhandariarnav2020@users.noreply.github.com>
2021-07-17 17:15:17 +0530

    Update main.js

--- main.js
+++ main.js
@@ -3,6 +3,6 @@ function onclick() {
   password=document.getElementById("password").value;
   if(password = "space213") {
     window.location='https://bhandariarnav2020.github.io/SRA/';
-  }
+  }}
     
     
